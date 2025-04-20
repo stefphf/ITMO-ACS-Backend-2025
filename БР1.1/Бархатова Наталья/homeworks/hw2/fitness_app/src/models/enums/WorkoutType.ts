d85f3649ab6870eb0abf8d21411c8f2e2b0d8064
Natalia Barkhatova <natabarkhatova@yandex.ru>
2025-04-20 18:40:07 +0300

    fix: clean master branch

--- БР1.1/Бархатова Наталья/homeworks/hw2/fitness_app/src/models/enums/WorkoutType.ts
+++
@@ -1,8 +0,0 @@
-export enum WorkoutType {
-    Cardio = "cardio",
-    Strength = "strength",
-    Flexibility = "flexibility",
-    Balance = "balance",
-    Mixed = "mixed",
-    Other = "other"
-  }
