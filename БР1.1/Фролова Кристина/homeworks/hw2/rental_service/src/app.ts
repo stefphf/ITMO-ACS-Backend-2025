bbb819e77dbf925556171a93e8061391a5fa74bc
Soudagh <fr.kristina@inbox.ru>
2025-04-20 17:50:31 +0300

    [fix]: Добавлен messageRouter в app.ts

--- БР1.1/Фролова Кристина/homeworks/hw2/rental_service/src/app.ts
+++ БР1.1/Фролова Кристина/homeworks/hw2/rental_service/src/app.ts
@@ -3,6 +3,7 @@ import dataSource from "./config/data-source";
 import userRouter from "./routes/user.router";
 import {errorHandler} from "./middleware/error.handler";
 import advertisementRouter from "./routes/advertisement.router";
+import messageRouter from "./routes/message.router";
 
 const app = express();
 
@@ -20,5 +21,6 @@ app.use(express.json());
 app.use('/users', userRouter);
 app.use('/advertisements', advertisementRouter);
 app.use('/categories', advertisementRouter);
+app.use('/messages', messageRouter);
 app.use(errorHandler);
 app.listen(3000, () => console.log("Server started on http://localhost:3000"));
