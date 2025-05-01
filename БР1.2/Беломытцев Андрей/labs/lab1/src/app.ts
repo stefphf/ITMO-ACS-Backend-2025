d0671f0f053a224177135339d669dae5f103d54f
Liberal <Liberal@MacBook-Air-Andrej-3.local>
2025-05-01 20:08:43 +0300

    lab-1

---
+++ БР1.2/Беломытцев Андрей/labs/lab1/src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express'
+import { AppDataSource } from "./AppDataSource"
+import categoryRouter from "./routers/Category"
+import channelRouter from "./routers/Channel"
+import reviewRouter from "./routers/Review"
+import roleRouter from "./routers/Role"
+import themeRouter from "./routers/Theme"
+import userRouter from "./routers/User"
+import videoRouter from "./routers/Video"
+import config from './config';
+
+const app = express()
+const PORT = config.APP_PORT
+
+app.use(express.json())
+app.use('/category', categoryRouter)
+app.use('/channel', channelRouter)
+app.use('/review', reviewRouter)
+app.use('/role', roleRouter)
+app.use('/theme', themeRouter)
+app.use('/user', userRouter)
+app.use('/video', videoRouter)
+
+AppDataSource.initialize().then(() => {
+  app.listen(PORT, () => {
+    console.log(`http://127.0.0.1:${PORT}`)
+  })
+}).catch(error => console.log(error))
