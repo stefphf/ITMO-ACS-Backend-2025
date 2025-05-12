6f8fc5fd4d868395154994a3482d78f7176825b6
Карина Красюк <karinakrasuk05@gmail.com>
2025-05-12 23:31:53 +0300

    feat: add lab1

---
+++ БР1.1/Красюк Карина/labs/lab1/src/app.ts
@@ -0,0 +1,34 @@
+﻿import express from "express"
+import dataSource from "./config/data-source"
+
+import { useExpressServer } from 'routing-controllers';
+import AuthController from "./controllers/auth.controller";
+import {UserController} from "./controllers/user.controller";
+
+dataSource
+    .initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization:", err)
+    })
+
+let app = express()
+app.use(express.json())
+
+const options = {
+    routePrefix: "/api",
+    controllers: [
+        UserController,
+        AuthController
+    ],
+    validation: true,
+    classTransformer: true,
+    defaultErrorHandler: true,
+};
+
+app = useExpressServer(app, options);
+
+
+app.listen(3000, () => console.log("Server started on http://localhost:3000"));
---
+++ БР1.1/Красюк Карина/labs/lab1/src/config/settings.ts
@@ -0,0 +1,22 @@
+﻿import { env } from 'process';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+class Settings {
+    DB_HOST = env.DB_HOST;
+    DB_PORT = parseInt(env.DB_PORT) || 5432;
+    DB_NAME = env.DB_NAME;
+    DB_USER = env.DB_USER;
+    DB_PASSWORD = env.DB_PASSWORD;
+    DB_ENTITIES = env.DB_ENTITIES || 'src/entities/*.ts';
+    
+    JWT_SECRET_KEY = env.JWT_SECRET_KEY || 'secret';
+    JWT_TOKEN_TYPE = env.JWT_SECRET_KEY || 'Bearer';
+    JWT_ACCESS_TOKEN_LIFETIME: number =
+        parseInt(env.JWT_ACCESS_TOKEN_LIFETIME) || 60 * 5;
+}
+
+const SETTINGS = new Settings();
+
+export default SETTINGS;
