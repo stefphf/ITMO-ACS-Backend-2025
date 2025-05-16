bc760f9f828cc4d8df108ebddd94ba771ca15686
arch1varius <112974779+arch1varius@users.noreply.github.com>
2025-05-16 22:34:08 +0300

    done

--- БР1.1/Петухов Семён Алексеевич/Lab1/job-platform-api/src/app.ts
+++
@@ -1,9 +0,0 @@
-mport express from "express";
-import basicRoutes from "./routes/basic.routes";
-
-const app = express();
-
-app.use(express.json());
-app.use("/api", basicRoutes); // Роуты будут доступны по /api/basic
-
-export default app;
