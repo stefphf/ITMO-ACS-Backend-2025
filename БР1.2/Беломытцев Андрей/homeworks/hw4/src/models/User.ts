28f0b30b2d56d50df1a7f87d867de6154f889e4f
Liberal <Liberal@MacBook-Air-Andrej-3.local>
2025-05-26 20:44:42 +0300

    hw4

---
+++ БР1.2/Беломытцев Андрей/homeworks/hw4/src/models/User.ts
@@ -0,0 +1,34 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany, CreateDateColumn } from 'typeorm'
+import { Channel } from './Channel'
+import { Review } from './Review'
+import { Role } from './Role'
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ type: 'varchar', length: 255, unique: true })
+  username: string
+
+  @Column({ type: 'varchar', length: 255, unique: true })
+  email: string
+
+  @Column({ type: 'varchar', length: 255})
+  password: string
+
+  @CreateDateColumn()
+  timeCreate: Date
+
+  @Column({ 'nullable': true })
+  about: string
+
+  @ManyToOne(type => Role, role => role.users)
+  role: Role
+
+  @OneToMany(type => Review, review => review.user)
+  reviews: Review[]
+
+  @OneToMany(type => Channel, channel => channel.user)
+  channels: Channel[]
+}
