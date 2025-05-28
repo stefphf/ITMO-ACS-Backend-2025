2335ca83d1f2429f8eb1f156ed09e62bd3afabb5
Dinara Khisametdinova <khdinova@gmail.com>
2025-05-28 14:49:12 +0300

    feat: some updates to Dockerfile and configss

--- БР1.2/Хисаметдинова Динара/labworks/lr3/psychologist_picking/apps/user/src/user/user.ms.controller.ts
+++ БР1.2/Хисаметдинова Динара/labworks/lr3/psychologist_picking/apps/user/src/user/user.ms.controller.ts
@@ -6,10 +6,8 @@ import { UserService } from '../user/user.service';
 export class UserMsController {
   constructor(private readonly userService: UserService) {}
 
-  // Получить пользователя по id (для reviews-service)
   @MessagePattern({ cmd: 'get-user-by-id' })
   async getUserById(userId: number) {
-    // Вернёт user или null
     return this.userService.findById(userId);
   }
 }
