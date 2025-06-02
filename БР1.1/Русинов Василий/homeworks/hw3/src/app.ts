eaff89a0cc80056de609cf677d5e9cdcd79dab79
vendatt0r <vasyok03020454@gmail.com>
2025-06-02 19:44:26 +0300

    feat: add hw3

---
+++ БР1.1/Русинов Василий/homeworks/hw3/src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import userRoutes from "./routes/user.routes";
+import resumeRoutes from "./routes/resume.routes";
+import employerRoutes from "./routes/employer.routes";
+import jobRoutes from "./routes/job.routes";
+import industryRoutes from "./routes/industry.routes";
+import applicationRoutes from "./routes/application.routes";
+import authRoutes from "./routes/auth.routes";
+import { setupSwagger } from './swagger';
+
+const app = express();
+
+app.use(express.json());
+app.use("/users", userRoutes);
+app.use("/resumes", resumeRoutes);
+app.use("/employers", employerRoutes);
+app.use("/jobs", jobRoutes);
+app.use("/industries", industryRoutes);
+app.use("/applications", applicationRoutes);
+app.use("/auth", authRoutes);
+
+setupSwagger(app);
+export default app;
